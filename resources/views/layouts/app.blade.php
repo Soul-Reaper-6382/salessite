632f446c7499278c5855815d1b999bfd39c5451d
Soul-Reaper-6382 <hhshahofficial@gmail.com>
2024-09-09 02:31:23 +0500

    back_nssssew

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>@yield('title', 'Smugglers')</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="Smuggler">
+    <meta name="description" content="Smugglers">
     <!-- Favicon -->
     <link rel="icon" href="{{ asset('favicon.png') }}">
 
