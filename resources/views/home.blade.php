a293f3e6d1b45387afcda49c1ba2629e3e2d476b
Soul-Reaper-6382 <hhshahofficial@gmail.com>
2024-10-23 15:49:56 +0500

    back_newssssssaaaa

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
             <div class="bg-white">
 
                      <div class="projects-wrapper projectinfotechno-bg section-space--pt_60" style="background-position: left;">
-                      <div class="container">
+                      <div class="container abc">
                         <div class="row">
                           <div class="col-lg-12">
                             <!-- section-title-wrap Start -->
